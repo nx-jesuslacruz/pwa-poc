fa0429539676f2f93243417790bc4e6f6963f8f2
Jesús S. Lacruz <jesus.lacruz@nexum.com>
2024-06-06 16:26:11 +0200

    face recogniiton pwa

--- faceRecognition/src/App.js
+++ faceRecognition/src/App.js
@@ -21,37 +21,31 @@ function App() {
           });
       }
 
-      async function loadModels() {
+      const run = async () => {
         await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+        await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
         await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
         await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-        await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
-      }
+        await faceapi.nets.faceExpressionNet.loadFromUri('/models');
 
-      async function handleVideoPlay() {
         const video = videoRef.current;
-        const canvas = await faceapi.createCanvasFromMedia(video);
+        const canvas = faceapi.createCanvasFromMedia(video);
         document.body.append(canvas);
         const displaySize = { width: video.width, height: video.height };
         faceapi.matchDimensions(canvas, displaySize);
-
-        // setInterval(async () => {
-          await loadModels();
-          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
-          console.log(detections)
-          const resizedDetections = faceapi.resizeResults(detections, displaySize);
-          const context = canvas.getContext('2d');
-          context.clearRect(0, 0, canvas.width, canvas.height);
-          faceapi.draw.drawDetections(canvas, resizedDetections);
-          faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-       // }, );
-      }
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      };
 
       startVideo();
 
-      videoRef.current.addEventListener('loadedmetadata', () => {
+      videoRef.current.addEventListener('canplay', () => {
         videoRef.current.play();
-        handleVideoPlay();
+        run();
       });
 
     }
